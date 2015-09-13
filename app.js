812825f8a7ad7027357dcd9dfee30cb5043ef2a7
Philip Riley <hiriley510@gmail.com>
2015-09-13 12:16:04 -0700

    plain w/ jquery

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   console.log('hey')
   $('.your-class').slick({
-    // setting-name: setting-value
+    // setting-name: 'setting-value',
+    autoplay:true,
+    autoplaySpeed:3000,
+    arrows:true,
+    dots:true
   });
 
 });
