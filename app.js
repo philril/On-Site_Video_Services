c9d990002d2488d046aaa235abbc7ec887b3f3eb
Philip Riley <philip_riley@apple.com>
2015-09-18 18:06:57 -0700

    adds separate pages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $('.your-class').slick({
     // setting-name: 'setting-value',
     autoplay:true,
+    infinite:true,
+    fade:true,
     autoplaySpeed:3000,
     arrows:true,
     dots:true
