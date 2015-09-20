92844054b3b3781030dcf29fb971bb360fbc34ea
Philip Riley <hiriley510@gmail.com>
2015-09-19 17:04:30 -0700

    Lots of changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
+
   console.log('hey')
   $('.your-class').slick({
     // setting-name: 'setting-value',
     autoplay:true,
     infinite:true,
     fade:true,
+    speed:2000,
     autoplaySpeed:3000,
     arrows:true,
     dots:true
